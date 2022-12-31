c84d5d41a063eaf0a473b378ee8ec711876ed645
rojashree <rojashree24@gmail.com>
2022-12-31 10:06:01 +0530

    done

---
+++ build/precache-manifest.e53f8b15243f905265b8e2b795cddf54.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ac1a6c7b6d5873acf1da3b2a886fca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91a8ce9d867e3d9aab4d",
+    "url": "/static/js/2.7358e7b1.chunk.js"
+  },
+  {
+    "revision": "7fe15a70a47ca10bc43e",
+    "url": "/static/js/main.97e60c6d.chunk.js"
+  },
+  {
+    "revision": "2f55955791534fe0d08b",
+    "url": "/static/js/runtime-main.1048f588.js"
+  }
+]);
